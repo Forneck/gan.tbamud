4f9eadbd7a5819a3dd4deb4d0720f262e988ba70
Fabiana <felipedaniel1984@gmail.com>
2024-03-15 11:00:47 -0300

    Impressao de Noise somente no verbose

--- gan.py
+++ gan.py
@@ -107,7 +107,8 @@ class GeneratorOutputDataset(Dataset):
 
     def __getitem__(self, idx):
         noise = torch.randint(0, self.noise_dim, (self.noise_samples , self.noise_dim))
-        print('Noise: ', noise)
+        if args.verbose == 'on':
+            print('Noise: ', noise)
         sample, _ = self.generator(noise)
         return sample
 
